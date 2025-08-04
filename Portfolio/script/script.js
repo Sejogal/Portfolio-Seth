a11bf63e27ab0214e642823ccc61d412a3adb07f
Seth Lussueki <sethlussueki@gmail.com>
2025-08-04 09:48:55 +0100

    Fazendo Upload do Portfolio
    Ainda incompleto!

---
+++ Portfolio/script/script.js
@@ -0,0 +1,21 @@
+//
+function iniciaSom(){
+    let audio = document.getElementById("audio");
+    audio.play();
+}
+iniciaSom();
+
+//
+const textWelcome = "Olá meu nome é Seth Lussueki Bem vindo ao meu PORTFÓLIO"
+const titulo = document.getElementById("welcome");
+let i = 0;
+
+function escrever(){
+    if(i < textWelcome.length){
+        titulo.innerHTML += textWelcome.charAt(i);
+        i++;
+        setTimeout(escrever,20);
+    }
+}
+escrever();
+
